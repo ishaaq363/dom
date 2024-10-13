c26682703414dafb00400ebe5b6500e5418ed14c
ishaaq363 <imatovu383@gmail.com>
2024-10-13 17:30:58 +0200

    validationform

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -3,7 +3,8 @@
 function validate(e) {
   e.preventDefault();
   var email = document.getElementById('email').value;
-  var pass = document.getElementById('password').value;
+  var password = document.getElementById('password').value;
+  var username = document.getElementById('username').value;
   var age = document.getElementById('age').value;
   var msgBox = document.getElementById('message');
   var message = '';
@@ -11,11 +12,14 @@ function validate(e) {
   if (email === '') {
     message = 'Please enter an email.';
     msgBox.style.color = 'red';
-  } else if (pass === '') {
+  } else if (password === '') {
     message = 'Password must be at least 8 characters.';
     msgBox.style.color = 'red';
   } else if (age === '') {
     message = 'Age must be between 12 and 50.';
+    msgBox.style.color = 'red';
+  } else if (username === '') {
+    message = 'Username must be at least 8 characters.';
     msgBox.style.color = 'red';
   } else {
     message = 'Login successful!';
--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function validate(e) {
     e.preventDefault();
 
     const email = document.getElementById('email').value;
-    const pass = document.getElementById('password').value;
+    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
     const age = document.getElementById('age').value;
     const msgBox = document.getElementById('message');
 
@@ -11,14 +12,16 @@ function validate(e) {
     if (email === '') {
         message = 'Please enter an email.';
         msgBox.style.color = 'red';
-    } else if (pass === '') {
+    } else if (password === '') {
         message = 'Password must be at least 8 characters.';
         msgBox.style.color = 'red';
     } else if (age === '') {
         message = 'Age must be between 12 and 50.';
         msgBox.style.color = 'red';
+    }  else if (username === '') {
+        message = 'Username must be at least 8 characters.';
+        msgBox.style.color = 'red';
     } 
-
     else {
         message = 'Login successful!';
         msgBox.style.color = 'green';
